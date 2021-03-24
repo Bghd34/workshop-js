777eb442f45d08aaa2880ce01462fc33b18568d7
yassine <boughadiyacine@yahoo.fr>
2021-03-24 09:20:48 +0100

    l'ajout des fichier

---
+++ js/01.function.js
@@ -0,0 +1,16 @@
+var nombre1=10;
+var ombre2 = 20;
+
+additionner = (nb1,nb2)=>{
+    somme = nb1+nb2;
+    return somme;
+};
+var resultat1 = additionner(15,15);
+
+console.log(resultat1);
+
+somme = additionner;
+resultat2 = somme(15,15)
+
+consle.log(resultat2);
+
